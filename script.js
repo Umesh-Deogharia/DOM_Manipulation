70f3be1c500441c6cf22617fe10171cfae096fe2
Umesh-Deogharia <umeshdeogharia07@gmail.com>
2024-09-24 23:31:29 +0530

    Updated

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let decrement = document.getElementById('decrement');
-let increment = document.getElementById('increment');
-let clear = document.getElementById('clear');
-let error = document.getElementById('error');
-function calc(e){
-    let counter = document.getElementById('para2');
-    let count = Number(counter.innerText);
-    // console.log(count);
-    console.log(e.target.id);
-    const {id}=e.target;
-    if(id === "increment"){
-        count++;
-    }else if(id === "decrement"){
-        if(count>0){
-            count--;
-        }
-    }else{
-        count = 0;
+let decrement = document.getElementById("decrement");
+let increment = document.getElementById("increment");
+let clear = document.getElementById("clear");
+let error = document.getElementById("error");
+function calc(e) {
+  let counter = document.getElementById("para2");
+  let count = Number(counter.innerText);
+  // console.log(count);
+  console.log(e.target.id);
+  const { id } = e.target;
+  if (id === "increment") {
+    count++;
+  } else if (id === "decrement") {
+    if (count > 0) {
+      count--;
     }
-    counter.innerText = count;
-    if(count === 0){
-        error.style.display = "block";
-        decrement.style.backgroundColor = "LightBlue";
-        decrement.style.color = "gray";
-        clear.style.display = "none";
-        decrement.style.cursor = "not-allowed"
-    }else{
-        error.style.display = "none"
-        clear.style.display = "block";
-         decrement.style.backgroundColor = "white";
-         decrement.style.color = "black";
-         decrement.style.cursor = "auto";
-    }
+  } else {
+    count = 0;
+  }
+  counter.innerText = count;
+  if (count === 0) {
+    error.style.display = "block";
+    decrement.style.backgroundColor = "LightBlue";
+    decrement.style.color = "gray";
+    clear.style.display = "none";
+    decrement.style.cursor = "not-allowed";
+  } else {
+    error.style.display = "none";
+    clear.style.display = "block";
+    decrement.style.backgroundColor = "white";
+    decrement.style.color = "black";
+    decrement.style.cursor = "auto";
+  }
 }
-document.getElementById('decrement').addEventListener('click',calc);
-document.getElementById('increment').addEventListener('click',calc);
-document.getElementById('clear').addEventListener('click',calc);
+document.getElementById("decrement").addEventListener("click", calc);
+document.getElementById("increment").addEventListener("click", calc);
+document.getElementById("clear").addEventListener("click", calc);
